1ffce41b0ac67dbbd59795ccf277aaac8a85fd51
DatzProject <hardilol92@gmail.com>
2025-08-22 06:04:25 +0800

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ ChartJS.register(
 );
 
 const endpoint =
-  "https://script.google.com/macros/s/AKfycbwrHZrcQE0zRf87sFHGNeHmcxMYnbVGowDIz1MgG3fJTkL1CgNHa1gBhnDFaB24i18q/exec";
+  "https://script.google.com/macros/s/AKfycbzbFAa4rxSuDk0dlG09hqlTao6hNVneo7L9gIvWJ-_P2g5Tnd8dGQhs5WMb7ZHoFl8rMQ/exec";
 const SHEET_SEMESTER1 = "RekapSemester1";
 const SHEET_SEMESTER2 = "RekapSemester2";
 
@@ -3620,7 +3620,7 @@ const SplashScreen: React.FC = () => {
         className="w-52 h-70 mb-4 animate-pulse-custom" //Pengaturan ukuran logo
       />
       <p className="text-gray-800 text-lg font-semibold mt-6">
-        Tunggu Sebentar
+        Roslina, S.Pd
       </p>
     </div>
   );
